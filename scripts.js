c124b7161d199121ce0555d93c344d967751ffe6
GO4ITJB <jovicabumbulovic@gmail.com>
2020-11-09 08:00:36 +0100

    about finished

--- scripts.js
+++ scripts.js
@@ -8,4 +8,30 @@ function handleMenuClick(event) {
   if (event.target instanceof HTMLAnchorElement) {
     closeIcon.checked = false;
   }
-}
+};
+
+// GSAP word animation
+const words = ['Jovica Bumbulovic', 'A gym enthusiast', 'A developer', 'Your next coworker'];
+let cursor = gsap.to('.hero__cursor', {opacity: 0,duration: 1, ease: 'power2.inOut', repeat: -1});
+
+let masterTL = gsap.timeline({repeat: -1, delay: .5});
+
+words.forEach(word => {
+  let tl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 1})
+  tl.to('.hero__h-anim', {duration: 1.1, text: word})
+  masterTL.add(tl)
+})
+
+// GSAP scroll trigger
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.start'
+  }
+});
+
+tl.from('.box', {
+  x: -50,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.4
+});
